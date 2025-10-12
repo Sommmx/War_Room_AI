6ced8ce94a35c9fd98e150a749a0d5ff16f1607e
Somnath Pichewar <somnathpichewar@gmail.com>
2025-10-12 20:31:29 +0530

    anomaly_detector file  done

--- src/anomaly_detector.py
+++ src/anomaly_detector.py
@@ -1,2 +1,21 @@
 import pandas as pd
 from .config import thresholds
+
+
+
+def metric_anomaly(df: pd.DataFrame):
+    anomalies = []
+    for _, row in df.iterrows():
+        metric = row['metric_name']
+        value = row['value']
+        service = row['service']
+
+        if metric in thresholds:
+            threshold = thresholds[metric]
+            if isinstance(threshold, dict):
+                threshold_value = threshold.get(service)
+            else:
+                threshold_value = threshold
+            if threshold_value is not None and value > threshold_value:
+                anomalies.append(row)
+    return pd.DataFrame(anomalies)
